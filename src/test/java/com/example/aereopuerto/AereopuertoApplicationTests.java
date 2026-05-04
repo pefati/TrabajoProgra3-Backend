7877a3aa995afc3ea0146f7b15bad8aeb6f8caad
pefati <francodavidrodriguez@gmail.com>
2026-05-04 08:51:06 -0300

    Dependencias iniciales

---
+++ src/test/java/com/example/aereopuerto/AereopuertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aereopuerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AereopuertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
