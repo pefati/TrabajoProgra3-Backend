3d4c076c42fd5e7b2e96c70f980d30b51b411270
valentinanavarro966-eng <valentinanavarro966@gmail.com>
2026-06-22 20:32:42 -0300

    Validacion en contraseña de ResetPasswordRequest

--- src/main/java/com/example/aereopuerto/auth/dto/ResetPasswordRequest.java
+++ src/main/java/com/example/aereopuerto/auth/dto/ResetPasswordRequest.java
@@ -1,6 +1,7 @@
 package com.example.aereopuerto.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,6 +17,9 @@ public class ResetPasswordRequest {
     private String token;
 
     @NotBlank
-    @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$",
+            message = "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un carácter especial"
+    )
     private String newPassword;
 }
